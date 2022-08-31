2c80971bdd77dbf65e3e175c7600755e72f6e9d4
Kvitkos <kvitkos99@yahoo.com>
2022-08-31 11:58:20 -0400

    Uploaded Practice files

---
+++ Practice/index.js
@@ -0,0 +1,21 @@
+console.log ("Week 3");
+/*
+Create a function named checkout that accepts 2 variables named cardBalance and price. 
+If the price is more than the card balance, return false.
+ Otherwise, return the new card balance.
+
+
+*/
+//create new function, 
+let a = 11;
+let b = 7;
+//// create 2 variables
+// cardBalance, price
+function Checkout(cardBalance, price){
+    console.log("this is = a and displays current cardBalance", cardBalance);
+}
+
+
+
+// function checkout taking two parameters a and b (cardBalance and price)
+Checkout(a,b)
